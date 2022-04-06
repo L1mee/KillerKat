a8627eebace2fe9ff363b2d86e70f026952f9cb5
L1mee <lukas.melsa@live.de>
2022-04-06 19:26:10 +0200

    idfk what i did
    i did what i do did

---
+++ Assets/Enemy/Scripts/Cat_StateMachine.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat_StateMachine : MonoBehaviour
+{
+    enum States
+    {
+        Idle,
+        Approach,
+        DemonicBite,
+        TailSweep,
+        Jump,
+        PawPunch
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
